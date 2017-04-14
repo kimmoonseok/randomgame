310133bb04b6bcbc0967965b4d806b68c24e3531
moonseokkim <william7895@naver.com>
2017-04-14 02:41:26 +0000

    first commit

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,9 +1,4 @@
 class GameController < ApplicationController
     def index
-        
-        my_percent = Array(1..100).to_a.sample(2).sort
-        
-        @my_percent = my_percent
-        
         end
 end
